a89a049eaa6df69e2a80c5759713ae7f951d1f04
Alexandr <sashylay0@gmail.com>
2022-10-06 11:50:07 +0300

    finish task01

--- task01.py
+++ task01.py
@@ -1,8 +1,19 @@
-#1. Задайте список, состоящий из произвольных чисел, количество задаёт пользователь.
-#Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётных позициях(не индексах).
+# 1. Задайте список, состоящий из произвольных чисел, количество задаёт пользователь.
+# Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётных позициях(не индексах).
 
+from itertools import count
 from random import sample
-def numList(count,):
-    list=sample(range(1, count * 2), count)
+
+
+def numList(count):
+    list = sample(range(1, count * 2), count)
     print(list)
-print(numList(int(input())))
+    i = 0
+    numSum = 0
+    while i <= count:
+        numSum += list[i]
+        i += 2
+    print(numSum)
+
+
+numList(int(input()))
